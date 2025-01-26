05044e093258a8b85240bb340160705747cfbd4a
Jude Agu <ijude236@gmail.com>
2025-01-26 16:18:12 +0100

    updates

--- app/components/Designs.jsx
+++ app/components/Designs.jsx
@@ -6,7 +6,7 @@ import { Web3Design } from './Web3Design'
 export const Designs = () => {
   return (
     <div
-     className='relative flex flex-col items-center  w-screen top-16 xl:top-16  md: space-y-10 md:space-y-16'>
+     className='relative flex flex-col items-center mx-auto w-screen top-16 xl:top-16  md: space-y-10 md:space-y-16'>
     <section className=''>
     <Web2Design />
     </section>
